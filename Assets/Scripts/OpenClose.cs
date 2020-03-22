24d16772a43ae89e4b3567a99cdbb2bcd5219d1a
Windows Letgoner <jmw686@nyu.edu>
2020-03-22 17:16:37 -0500

    Better chest open, but still not great.

--- Assets/Scripts/OpenClose.cs
+++ Assets/Scripts/OpenClose.cs
@@ -9,7 +9,7 @@ public class OpenClose : MonoBehaviour
     void Start()
     {
         toggleChest = gameObject.GetComponent<Animator>();
-
+        toggleChest.SetBool("IsClosed", true);
     }
 
     // Update is called once per frame
@@ -20,23 +20,32 @@ public class OpenClose : MonoBehaviour
     }
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
+    //    Debug.Log("entered");
 //        Animator toggleChest = other.gameObject.GetComponent<Animator>();
         if (Input.GetKey(KeyCode.Space))
         {
-            //Reset the "Crouch" trigger
+            bool closed = toggleChest.GetBool("IsClosed");
+            if (closed)
+            {
             toggleChest.ResetTrigger("IsClosed");
-
-            //Send the message to the Animator to activate the trigger parameter named "Jump"
             toggleChest.SetTrigger("IsOpen");
+            } else
+            {
+            toggleChest.ResetTrigger("IsOpen");
+            toggleChest.SetTrigger("IsClosed");
+
+            }
+            //Reset the "Crouch" triggerd
+            //Send the message to the Animator to activate the trigger parameter named "Jump"
         }
 
         if (Input.GetKey("z"))
         {
             //Reset the "Jump" trigger
             toggleChest.ResetTrigger("IsOpen");
-
+            Debug.Log("ez");
             //Send the message to the Animator to activate the trigger parameter named "Crouch"
             toggleChest.SetTrigger("IsClosed");
         }
